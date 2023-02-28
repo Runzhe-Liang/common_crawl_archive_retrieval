6ae8cd39575bb1a5edad517498b161addd86a9ba
runzhe liang <longhair@Runzhes-MacBook-Pro.local>
2023-02-27 22:32:14 -0500

    modified README.md, add main.py

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # common_crawl_archive_retrieval
+
+The main part of this python crawler is `main.py`, which is based on the provided basic script from **code402**.
+The `results.txt` contains all the selected URLs, categorized by the month of the `WET` file they belong to.
+
+The crawler works by setting up correct HTTPS path to retrieve `WET` files from the web, and then analyze the 
+webpages one by one. The reason we use the `WET` files instead of `WARC` files is because `WET` files are, after 
+a fair amount of testing, more relevant to the contents of the page and can result in higher retrieval precision. 
+
+We record a web page as long as it as at least 20 occurrences of *COVID* related keywords and 10 occurrences of
+*economy* related keywords. We also conduct a pre-filtering to exclude all URLs that do not contain `.com` and 
+all URLs that may contain excessive information and thus abuse our retrieval standards (e.g. pages from yahoo.news 
+tend to do this as each of them contains lot of "side-information" that doesn't actually belong to the main content
+of the webpage). The pre-filtering improves the retrieval accuracy significantly as well. 
+
+Due to the time limit, more experiments could not be carried out. For example, we can design more elaborated 
+boolean queries to achieve higher retrieval precision and get more relevant webpages, based on our ablations. The
+current retrieval list is not ranked. We can use different metrics to order the retrieval results if that's wanted.
+A simple metric would be the total number of occurrences of *COVID* related keywords plus *economy* related keywords.
+
+To diversify the retrieval result, also to decrease retrieval time, we set an upper bound of documents that each 
+month can contribute. An alternative way could be getting all the relevant documents first, and then rank them 
+according to some universal standard, and take the top n documents.
---
+++ main.py
@@ -0,0 +1,126 @@
+from warcio.archiveiterator import ArchiveIterator
+import re
+import requests
+
+
+reg_econ = re.compile(" economic| Economic| economics| Economics| economy| Economy")
+reg_covid = re.compile(" covid |Covid|COVID|COVID-19|Coronavirus|coronavirus|pandemic|Pandemic")
+months = ["January 2020: \n\n", "\n\nFebruary 2020\n\n", "\n\nMarch/April 2020\n\n", "\n\nMay\June 2020\n\n",
+          "\n\nJuly 2020\n\n", "\n\nAugust 2020\n\n", "\n\nSeptember 2020\n\n", "\n\nOctober 2020\n\n",
+          "\n\nNovember/December 2020\n\n"]
+
+path_header = "https://data.commoncrawl.org/"
+
+month_counter = 0
+
+def retrieve(file_name):
+    stream = None
+    entries = 0
+    matching_entries = 0
+    hits = 0
+
+    global month_counter
+
+    if file_name.startswith("http://") or file_name.startswith(
+        "https://"
+    ):
+        stream = requests.get(file_name, stream=True).raw
+    else:
+        stream = open(file_name, "rb")
+
+    for record in ArchiveIterator(stream):
+        covid_cnt = 0
+        econ_cnt = 0
+
+        if record.rec_type == "warcinfo":
+            continue
+
+        if not ".com/" in record.rec_headers.get_header(
+                "WARC-Target-URI"
+        ):
+            continue
+
+        if "yahoo" in record.rec_headers.get_header(
+                "WARC-Target-URI"
+        ):
+            continue
+
+        entries = entries + 1
+
+        contents = (
+            record.content_stream()
+            .read()
+            .decode("utf-8", "replace")
+        )
+
+        find_covid = reg_covid.search(contents)
+        find_econ = reg_econ.search(contents)
+
+        if (find_covid and find_econ):
+            hits = hits + 1
+            covid_cnt += 1
+            econ_cnt += 1
+            find_covid = reg_covid.search(contents, find_covid.end())
+            find_econ = reg_econ.search(contents, find_econ.end())
+
+
+        while find_covid:
+            find_covid = reg_covid.search(contents, find_covid.end())
+            hits = hits + 1
+            covid_cnt += 1
+
+            if (covid_cnt >= 20):
+                break
+
+        while find_econ:
+            find_econ = reg_econ.search(contents, find_econ.end())
+            hits = hits + 1
+            econ_cnt += 1
+
+            if (econ_cnt >= 10):
+                break
+
+        if (covid_cnt >= 20 and econ_cnt >= 10):
+            f = open('results.txt', 'a')
+            f.write(record.rec_headers.get_header("WARC-Target-URI") + '\n')
+            f.close()
+            matching_entries = matching_entries + 1
+            month_counter += 1
+
+
+    print(
+        "Python: "
+        + str(hits)
+        + " matches in "
+        + str(matching_entries)
+        + "/"
+        + str(entries)
+    )
+
+def main():
+    for i in range(5):
+        file = open('results.txt', 'a')
+        file.write(months[5 - i])
+        file.close()
+
+        global month_counter
+
+        month_counter = 0
+
+        file = open('wet.paths' + str(5 - i), 'r')
+        paths = file.readlines()
+
+        for path in paths:
+            file_name = path_header + path[:-1]
+            print(file_name)
+            retrieve(file_name)
+
+            if (month_counter >= 200):
+                break
+
+        file.close()
+
+
+
+
+main()
